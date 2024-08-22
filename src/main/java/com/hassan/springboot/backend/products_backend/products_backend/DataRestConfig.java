487d9b6a5fe63f1879578f2d5449e48a8882f38a
Hassan Abbud <hassanabbud@hotmail.com>
2024-08-22 12:54:03 -0600

    Expose Ids to rest services

---
+++ src/main/java/com/hassan/springboot/backend/products_backend/products_backend/DataRestConfig.java
@@ -0,0 +1,18 @@
+package com.hassan.springboot.backend.products_backend.products_backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import com.hassan.springboot.backend.products_backend.products_backend.entities.Product;
+
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer{
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(Product.class);    
+    }
+
+}
--- src/main/java/com/hassan/springboot/backend/products_backend/products_backend/repositories/ProductRepository.java
+++ src/main/java/com/hassan/springboot/backend/products_backend/products_backend/repositories/ProductRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.hassan.springboot.backend.products_backend.products_backend.entities.Product;
 
+// Automatically create Get, Update, Insert, Delete, CRUD methods
 @RepositoryRestResource(path = "products")
 public interface ProductRepository extends CrudRepository<Product, Long>{
 
